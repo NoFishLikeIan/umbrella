4ee78f86f561fb9f5c76d933da7f9429d8d26e83
NoFishLikeIan <andreatitton96@gmail.com>
2020-04-25 17:05:29 +0200

    feat(transducers-stats): init tests

--- packages/transducers-stats/test/index.ts
+++ packages/transducers-stats/test/index.ts
@@ -1,6 +1,18 @@
 // import * as assert from "assert";
 // import * as transducers-stats from "../src/index";
+import { kalman } from '../src/index'
 
 describe("transducers-stats", () => {
+    it("tests pending");
+});
+
+
+describe("kalman", () => {
+    console.log("dudee")
+    const result = kalman(0, 10e7, [1, 2, 3])
+
+    console.log(result)
+
+
     it("tests pending");
 });
