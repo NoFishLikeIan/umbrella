5930ced478e65747b7184a65000314b6dd95f355
nkint <alberto.massa@ymail.com>
2020-04-25 23:52:54 +0200

    improvement(transducers-stats): kalman filter as transducer

--- packages/transducers-stats/src/kalman.ts
+++ packages/transducers-stats/src/kalman.ts
@@ -1,38 +1,31 @@
-import { Reducer, $$reduce } from "@thi.ng/transducers";
+import { Reducer, Transducer, $iter, compR } from "@thi.ng/transducers";
 
 type KalmanStep = [number, number];
 
-function step(x: number, a: number, p: number) {
-    const error = x - a;
-    const errorVariance = p + 1;
-
-    const K = p / errorVariance;
-
-    const aPrime = a + K * error;
-    const pPrime = p * (1 - K) + 1;
-
-    return [aPrime, pPrime] as KalmanStep;
-}
-
-export function kalman(a: number, p: number): Reducer<KalmanStep[], number>;
+export function kalman(a: number, p: number): Transducer<number, KalmanStep>;
 export function kalman(
     a: number,
     p: number,
-    xs: Iterable<number>
-): Reducer<KalmanStep[], number>;
+    src: Iterable<number>
+): IterableIterator<KalmanStep>;
+export function kalman(
+    a: number,
+    p: number,
+    src?: Iterable<number>
+): IterableIterator<KalmanStep>;
 export function kalman(...args: any[]): any {
-    const res = $$reduce(kalman, args);
-    if (res !== undefined) {
-        return res;
-    }
-    const [a, p] = args;
-    return [
-        () => [[a, p]],
-        (acc: any) => acc,
-        (acc: any, x: any) => {
-            const last = acc[acc.length - 1];
-            acc.push(step(x, last[0], last[1]));
-            return acc;
-        },
-    ];
+    return (
+        $iter(kalman, args) ||
+        ((rfn: Reducer<any, KalmanStep>) => {
+            let [a, p] = <number[]>args;
+            return compR(rfn, (acc: any, x: number) => {
+                const error = x - a;
+                const errorVariance = p + 1;
+                const K = p / errorVariance;
+                a += K * error;
+                p = p * (1 - K) + 1;
+                return rfn[2](acc, [a, p]);
+            });
+        })
+    );
 }
